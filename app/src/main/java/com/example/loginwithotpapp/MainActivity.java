72982e66a6f495bb036b60542537079cf4c7b9e5
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-01 23:51:59 +0530

    set functionality for button

--- app/src/main/java/com/example/loginwithotpapp/MainActivity.java
+++ app/src/main/java/com/example/loginwithotpapp/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.loginwithotpapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.hbb20.CountryCodePicker;
 
@@ -19,5 +21,20 @@ public class MainActivity extends AppCompatActivity {
         e = findViewById(R.id.editText1);
         b = findViewById(R.id.button1);
         ccp = findViewById(R.id.ccp);
+        ccp.registerCarrierNumberEditText(e);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(e.getText().toString().isEmpty()){
+                    e.setError("Please fill phone number");
+                }
+                else if(e.getText().toString().length()!=6){
+                    e.setError("OTP not valid");
+                }
+                else{
+
+                }
+            }
+        });
     }
 }
