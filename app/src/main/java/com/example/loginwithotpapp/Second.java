89cd5dd99de304896b47d6f6077e94a418044908
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-02 00:08:17 +0530

    get variables for views and initialize them in second activity.

--- app/src/main/java/com/example/loginwithotpapp/Second.java
+++ app/src/main/java/com/example/loginwithotpapp/Second.java
@@ -3,12 +3,22 @@ package com.example.loginwithotpapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Second extends AppCompatActivity {
-
+    Button b;
+    EditText e;
+    FirebaseAuth f;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        b = findViewById(R.id.button2);
+        e = findViewById(R.id.editText2);
+        f = FirebaseAuth.getInstance();
+
     }
 }
