11f6643538d5a44c67513a09b0202eb692dc9c91
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-01 23:44:41 +0530

    declare variables for main activity.

--- app/src/main/java/com/example/loginwithotpapp/Third.java
+++ app/src/main/java/com/example/loginwithotpapp/Third.java
@@ -3,9 +3,15 @@ package com.example.loginwithotpapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.hbb20.CountryCodePicker;
 
 public class Third extends AppCompatActivity {
-
+    EditText e;
+    Button b;
+    CountryCodePicker ccp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
